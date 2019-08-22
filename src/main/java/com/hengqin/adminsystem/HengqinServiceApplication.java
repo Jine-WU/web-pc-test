c2edfa3e209465bd519b453cd0d200c2677c58bc
Jacken <870964443@qq.com>
2019-08-22 22:45:39 +0800

    提交项目框架

---
+++ src/main/java/com/hengqin/adminsystem/HengqinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hengqin.adminsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HengqinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HengqinServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hengqin/adminsystem/HengqinServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hengqin.adminsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HengqinServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
