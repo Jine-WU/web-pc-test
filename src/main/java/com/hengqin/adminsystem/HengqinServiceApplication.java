2f66456fa612cc4ff779d582df652c1f4fb17fda
Jacken <870964443@qq.com>
2019-08-22 23:21:10 +0800

    feat（项目初始化）：项目初始化

--- src/main/java/com/hengqin/adminsystem/HengqinServiceApplication.java
+++ src/main/java/com/hengqin/adminsystem/HengqinServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HengqinServiceApplication {
 	/*程序入口*/
+    // 注释
 	public static void main(String[] args) {
 		SpringApplication.run(HengqinServiceApplication.class, args);
 	}
